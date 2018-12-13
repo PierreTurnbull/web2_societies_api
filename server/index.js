ea78014accb574c7639480bd0367274d8855e3bb
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-12-13 17:25:02 +0100

    Added mail POST endpoint.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const dotenvConfig = require('dotenv').config()
 const express = require('express');
 const isProduction = process.env.NODE_ENV === 'production';
 const nodemailer = require('nodemailer');
@@ -10,6 +11,37 @@ if (dotenvConfig.error) {
 
 const app = express();
 
+// POST mail endpoint
+app.post('/email', function(req, res) {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+      }
+    });
+
+    const mailOptions = {
+      from: '',
+      to: 'pierre.turnbull42@gmail.com',
+      subject: 'Sending Email using Node.js',
+      text: 'That was easy!'
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+    res.status(200).json('ok');
+  } catch (error) {
+    res.status(500).json('The email couldn\'t be sent');
+  }
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var error = new Error('Not Found');
@@ -19,7 +51,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(error, req, res, next) {
-  if (isProduction) { console.log(err.stack) };
+  if (isProduction) { console.log(error.stack) };
   res.status(error.status || 500);
   res.json({'errors': {
     message: error.message,
