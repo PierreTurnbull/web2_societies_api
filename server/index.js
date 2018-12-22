3c7f0c708d3ef93e43a89fce47789e2a50650ea9
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-12-22 17:46:48 +0100

    Send mail with request data.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,32 @@
 const dotenvConfig = require('dotenv').config()
-const express = require('express');
-const isProduction = process.env.NODE_ENV === 'production';
-const nodemailer = require('nodemailer');
-
-// if the environment variables weren't succesfully retrieved, the server should not be started
-if (dotenvConfig.error) {
-  console.log('Couldn\'t start the server:', dotenvConfig.error, '\n');
-  process.exit(1);
+const express = require('express')
+const bodyParser = require('body-parser')
+const isProduction = process.env.NODE_ENV === 'production'
+const nodemailer = require('nodemailer')
+const cors = require('cors');
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Only allow requests from the the front-office
+    if (origin === process.env.FO_ROOT_URL) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
 }
 
-const app = express();
+// If the environment variables weren't succesfully retrieved, the server should not be started
+if (dotenvConfig.error) {
+  console.log(`Could not start the server: ${dotenvConfig.error}\n`)
+  process.exit(1)
+}
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(cors(corsOptions));
 
 // POST mail endpoint
 app.post('/email', function(req, res) {
@@ -20,46 +37,57 @@ app.post('/email', function(req, res) {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS
       }
-    });
+    })
+
+    const formattedMessage = `${req.body.message}
+    
+    -----
+    
+    This email was sent by ${req.body.name || 'anonymous'} from your webdoc API.`
 
     const mailOptions = {
       from: '',
-      to: 'pierre.turnbull42@gmail.com',
-      subject: 'Sending Email using Node.js',
-      text: 'That was easy!'
-    };
+      to: process.env.FEEDBACK_MAILING_LIST,
+      subject: 'Webdoc feedback',
+      text: formattedMessage
+    }
 
-    transporter.sendMail(mailOptions, function(error, info){
+    transporter.sendMail(mailOptions, function(error, info) {
       if (error) {
-        console.log(error);
+        throw new Error(error)
       } else {
-        console.log('Email sent: ' + info.response);
+        res.status(200).json(info.response)
       }
-    });
-    res.status(200).json('ok');
+    })
   } catch (error) {
-    res.status(500).json('The email couldn\'t be sent');
+    res.status(500).json('The email couldn\'t be sent')
   }
-});
+})
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var error = new Error('Not Found');
-  error.status = 404;
-  next(error);
-});
+  const error = new Error('Endpoint not Found')
+  error.status = 404
+  next(error)
+})
 
-// error handler
+// Error handler
 app.use(function(error, req, res, next) {
-  if (isProduction) { console.log(error.stack) };
-  res.status(error.status || 500);
-  res.json({'errors': {
-    message: error.message,
-    error: isProduction ? {} : error
-  }});
-});
+  const errorInfo = {
+    'errors': {
+      message: error.message,
+      error: error
+    }
+  }
+  // Debug logs should never be output in production
+  if (!isProduction) {
+    console.log(error.stack)
+  }
+  res.status(error.status || 500)
+  res.json(isProduction ? {} : errorInfo)
+})
 
 // start server
 const server = app.listen(3001, function() {
-  console.log('Listening on port ' + server.address().port);
-});
+  console.log('Listening on port ' + server.address().port)
+})
